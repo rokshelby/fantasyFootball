219504f494a5bdd9af7f2753b884fe067a84f81d
rakeemshelby <rokshelby@gmail.com>
2026-01-02 17:52:25 -0600

    Initial clean commit (no videos)

---
+++ script/gallery.js
@@ -0,0 +1,39 @@
+
+let currentType = "photos";
+
+async function loadMedia() {
+  const year = document.getElementById("yearSelect").value;
+  const res = await fetch(`../gallery/${year}/manifest.json`);
+  const data = await res.json();
+
+  const gallery = document.getElementById("gallery");
+  gallery.innerHTML = "";
+
+  data[currentType].forEach(file => {
+    if (currentType === "photos") {
+      gallery.innerHTML += `
+        <img src="../gallery/${year}/${file}" loading="lazy">
+      `;
+    } else {
+      gallery.innerHTML += `
+        <video controls preload="metadata">
+          <source src="../gallery/${year}/${file}">
+        </video>
+      `;
+    }
+  });
+}
+
+document.querySelectorAll(".tab").forEach(tab => {
+  tab.addEventListener("click", () => {
+    document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
+    tab.classList.add("active");
+    currentType = tab.dataset.type;
+    loadMedia();
+  });
+});
+
+document.getElementById("yearSelect").addEventListener("change", loadMedia);
+
+loadMedia();
+
